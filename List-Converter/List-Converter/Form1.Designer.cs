ca9a11c7119219d1231392c3461bc185aab333dc
Alexander Lairich <alexanderlairich@web.de>
2023-01-18 13:11:40 +0100

    v1
    Converting from list to string and other way around.
    
    Split the data into useable form now.

---
+++ List-Converter/List-Converter/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿
+namespace List_Converter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.InitialTextBox = new System.Windows.Forms.RichTextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.OpenInitialListBtn = new System.Windows.Forms.Button();
+            this.ClearInitialListBtn = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.ConvertBtn = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.ConvertedTextBox = new System.Windows.Forms.RichTextBox();
+            this.StringTextBox = new System.Windows.Forms.RichTextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // InitialTextBox
+            // 
+            this.InitialTextBox.Location = new System.Drawing.Point(6, 49);
+            this.InitialTextBox.Name = "InitialTextBox";
+            this.InitialTextBox.Size = new System.Drawing.Size(643, 163);
+            this.InitialTextBox.TabIndex = 0;
+            this.InitialTextBox.Text = "";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.ConvertBtn);
+            this.groupBox1.Controls.Add(this.ClearInitialListBtn);
+            this.groupBox1.Controls.Add(this.OpenInitialListBtn);
+            this.groupBox1.Controls.Add(this.InitialTextBox);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(655, 220);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Initial List";
+            // 
+            // OpenInitialListBtn
+            // 
+            this.OpenInitialListBtn.Location = new System.Drawing.Point(6, 19);
+            this.OpenInitialListBtn.Name = "OpenInitialListBtn";
+            this.OpenInitialListBtn.Size = new System.Drawing.Size(75, 23);
+            this.OpenInitialListBtn.TabIndex = 1;
+            this.OpenInitialListBtn.Text = "Open";
+            this.OpenInitialListBtn.UseVisualStyleBackColor = true;
+            // 
+            // ClearInitialListBtn
+            // 
+            this.ClearInitialListBtn.Location = new System.Drawing.Point(87, 19);
+            this.ClearInitialListBtn.Name = "ClearInitialListBtn";
+            this.ClearInitialListBtn.Size = new System.Drawing.Size(75, 23);
+            this.ClearInitialListBtn.TabIndex = 2;
+            this.ClearInitialListBtn.Text = "Clear";
+            this.ClearInitialListBtn.UseVisualStyleBackColor = true;
+            this.ClearInitialListBtn.Click += new System.EventHandler(this.ClearInitialListBtn_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.StringTextBox);
+            this.groupBox2.Location = new System.Drawing.Point(12, 238);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(655, 170);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "String Data";
+            // 
+            // ConvertBtn
+            // 
+            this.ConvertBtn.Location = new System.Drawing.Point(168, 19);
+            this.ConvertBtn.Name = "ConvertBtn";
+            this.ConvertBtn.Size = new System.Drawing.Size(75, 23);
+            this.ConvertBtn.TabIndex = 3;
+            this.ConvertBtn.Text = "Convert";
+            this.ConvertBtn.UseVisualStyleBackColor = true;
+            this.ConvertBtn.Click += new System.EventHandler(this.ConvertBtn_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.ConvertedTextBox);
+            this.groupBox3.Location = new System.Drawing.Point(12, 414);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(655, 167);
+            this.groupBox3.TabIndex = 4;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Converted data";
+            // 
+            // ConvertedTextBox
+            // 
+            this.ConvertedTextBox.Location = new System.Drawing.Point(6, 19);
+            this.ConvertedTextBox.Name = "ConvertedTextBox";
+            this.ConvertedTextBox.Size = new System.Drawing.Size(643, 142);
+            this.ConvertedTextBox.TabIndex = 0;
+            this.ConvertedTextBox.Text = "";
+            // 
+            // StringTextBox
+            // 
+            this.StringTextBox.Location = new System.Drawing.Point(6, 19);
+            this.StringTextBox.Name = "StringTextBox";
+            this.StringTextBox.Size = new System.Drawing.Size(643, 142);
+            this.StringTextBox.TabIndex = 1;
+            this.StringTextBox.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(679, 589);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "List Converter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox InitialTextBox;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button OpenInitialListBtn;
+        private System.Windows.Forms.Button ClearInitialListBtn;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button ConvertBtn;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.RichTextBox ConvertedTextBox;
+        private System.Windows.Forms.RichTextBox StringTextBox;
+    }
+}
+
---
+++ List-Converter/List-Converter/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace List_Converter
+{
+    public partial class Form1 : Form
+    {
+        public List<string> initalList;
+        public string buildString;
+        public List<string> convertedList;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            initalList = new List<string>();
+            convertedList = new List<string>();
+            buildString = string.Empty;
+
+            initalList.Add("Test1:Test2");
+            initalList.Add("Test2:Test3");
+            initalList.Add("Test3:Test4");
+        }
+
+        private void ClearInitialListBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ConvertBtn_Click(object sender, EventArgs e)
+        {
+            // Convert initial list to stringdata
+            StringTextBox.Text = ConvertListToString(initalList);
+
+
+            // SEND THE STRINGDATA TO CLIENT NOW VIA HEADER ETC...
+
+
+            // On the client side convert the string
+            SplitStringIntoValues();
+
+
+            // Add converted data to a list and compare
+            ConvertStringToList(convertedList, StringTextBox.Text);
+
+        }
+
+
+        public string ConvertListToString(List<string> ListToConvert)
+        {
+            // definde string to return
+            string stringData = string.Empty;
+
+            // loop through list and build string
+            foreach (string entry in ListToConvert)
+            {
+                // get entry
+                string entryID = ListToConvert.IndexOf(entry).ToString();
+                stringData += entry + "\n";
+            }
+
+            // return string
+            return stringData;
+        }
+
+
+        public void SplitStringIntoValues()
+        {
+
+        }
+
+
+        public void ConvertStringToList(List<string> ConvertedList, string StringToConvert)
+        {
+            using (StringReader reader = new StringReader(StringToConvert))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    ConvertedList.Add(line + "\n");
+                }
+            }
+        }
+    }
+}
